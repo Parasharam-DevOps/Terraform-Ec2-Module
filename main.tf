6a303c96ca939ef98860852c304e9430e626fd0a
Parasharam Desai <132131379+Parasharam-DevOps@users.noreply.github.com>
2024-02-29 10:24:11 +0530

    Update main.tf
    Update the root volume

--- main.tf
+++ main.tf
@@ -1,19 +1,20 @@
-# Configure the AWS Provider
-provider "aws" {
-  region = "eu-central-1"
-}
+
 
 /*--------------- Public SG Module---------------*/
 
 module "ec2" {
-  source = "github.com/Parasharam-DevOps/Terraform-Ec2-Module.git//ec2_module"
+  source                = "github.com/Parasharam-DevOps/Terraform-Ec2-Module.git//ec2_module"
+
+  # Pass variables for region
+  region_code = "us-west-2"
 
   # Pass variables to the module
   key_pair              = "private-key"
-  ami_id                = "ami-0faab6bdbac9486fb"
+  ami_id                = "ami-008fe2fc65df48dac"
   public_instance_count = 2
   public_instance_type  = "t2.micro"
   public_instance_name  = "public-instance"
+  root_volume_size      = "16"
 }
 
 /*--------------- Output Values ---------------*/
